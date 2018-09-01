916fac0daf03a4d80732b5913433b323a92ed33f
rob10thHuman <rob@10thHuman.com>
2018-09-01 16:54:02 -0600

    added more lines of code to CashRegister

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -17,18 +17,32 @@ public class CashRegister {
 //		double changeGiven = amountGiven - itemPrice; 
 //		double changeGiven; 
 		
-		if (amountGiven < itemPrice) {
-			System.out.println("You need more money! ");
-		}
-		else if (amountGiven == itemPrice) {
-//			System.out.println("Exact price given! ");
-			exactPrice();
-		}
-		else {			
-//			System.out.println(changeGiven);
-			doTheMath(itemPrice, amountGiven); 
-		}
+		//talking to my employing broker and describing the problem, it occurred to me 
+		//that the computer doesn't know what a penny, etc., is so...
 		
+		double penny=.01, nickel=.05, dime=.1, quarter=.25, dollar=1.00, fiveDollar=5.00, tenDollars=10.00; 
+		double twenty=20.00, fifty=50.00, hundred=100.00;
+		
+		//testing
+		double changeOwed=amountGiven - itemPrice - hundred; 
+		System.out.println(changeOwed);
+		
+		//I think I need some form of loop
+		//have to think of a way that change owed is tested against the balance, etc 
+		
+		
+//		if (amountGiven < itemPrice) {
+//			System.out.println("You need more money! ");
+//		}
+//		else if (amountGiven == itemPrice) {
+////			System.out.println("Exact price given! ");
+//			exactPrice();
+//		}
+//		else {			
+////			System.out.println(changeGiven);
+//			doTheMath(itemPrice, amountGiven); 
+//		}
+//		
 	}
 
 	
